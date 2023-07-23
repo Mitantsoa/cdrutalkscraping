331bf88142ff7ca6f3f5a902a9f9e3267375e018
unknown <mitantsoa.andrianjaka@gmail.com>
2023-07-23 12:42:04 +0300

    test bot

--- index.js
+++ index.js
@@ -2,24 +2,50 @@ const {config} = require("dotenv");
 const login = require("./login.js");
 const express = require("express");
 const {logger} = require("./src/service/utils");
-const cronClass = require("./indexcron")
-const cron = new cronClass();
+const cron = require("./indexcronv2")
 const app = express();
 const db = require('./src/service/mysqlAddCdr')
+const cronJson = require('./cron.json')
+const moment = require("moment");
 
 config();
 
-app.get('/startcron',async (req,res)=> {const resp = await cron.start();res.send(resp);})
-app.get('/stopcron',async (req,res)=> {const resp = cron.stop();res.send(resp);})
-app.get('/',(req,res)=>{
-    const resp = {
-        status:cron.isrunning?"running":"stoped",
-        startDate:cron.startDate,
-        NbHit:cron.countOfHit,
-        CronFormat:cron.cronFormat,
-        endDate:cron.endDate
+app.get('/startcron',async (req,res)=> {
+    if(!cronJson.isrunning){
+        let resp = cronJson;
+        try{
+            await cron.start();
+            cronJson.startDate = moment().format();
+            cronJson.countOfHit = 0;
+            cronJson.isrunning = true;
+            resp = cronJson;
+        }catch (e) {
+            console.log(e)
+            resp = e;
+        }
+        res.send(resp);
+    }else{
+        res.send("Cron is already running");
     }
-    res.send(resp);
+
+})
+app.get('/stopcron',async (req,res)=> {
+    if(cronJson.isrunning){
+        try{
+            await cron.stop();
+            cronJson.endDate = moment().format();
+            cronJson.isrunning = false;
+            res.send(cronJson);
+        }catch (e) {
+            res.send(e);
+        }
+    }else{
+        res.send("Cron is not running")
+    }
+
+})
+app.get('/',(req,res)=>{
+    res.send(cronJson);
 })
 
 app.get('/select',async (req,res)=>{
@@ -29,7 +55,7 @@ app.get('/select',async (req,res)=>{
 
 app.listen(80,async (socket)=>{
     logger("### Node app start ###")
-    await cron.start()
+    // await cron.start()
     // socket.keepAliveTimeout = (60 * 1000) + 1000;
     // socket.headersTimeout = (60 * 1000) + 2000;
 });
--- indexcronv2.js
+++ indexcronv2.js
@@ -3,7 +3,7 @@ const {logger} = require("./src/service/utils");
 const cron = require('node-cron');
 const moment = require("moment");
 const {config} = require("dotenv");
-const cronJson = require("cron.json");
+const cronJson = require("./cron.json");
 config();
 class cronClass{
     c;
@@ -21,29 +21,30 @@ class cronClass{
             this.countOfHit = 0;
             this.endDate = "--";
             this.startDate = moment().format();
-            this.c = cron.schedule(this.cronFormat, async () => {
-                logger("Application started");
-                this.countOfHit++;
-                let isLogedIn = false;
-                isLogedIn = await login()
-                // logger("we cron ---")
-            });
+            this.c =
             this.isrunning = true;
 
             return "cron is started"
         }else return "cron is already running"
     }
 
-    stop(){
-        if(this.isrunning){
-            logger("cron stoped")
-            this.endDate = moment().format()
-            this.c.stop()
-            this.isrunning = false
-            return "cron stoped"
-        }else return "cron not yet started"
-    }
 }
+
+const cronFormat = process.env.CRONFORMAT.replaceAll("e","*");
+const scheduleOptions = {
+    scheduled: true,
+    timezone: 'Europe/Paris',
+    name: 'cdr-fetch',
+    recoverMissedExecutions: true,
+};
+
+const action = async () =>{
+    logger("Application started");
+    this.countOfHit++;
+    let isLogedIn = false;
+    isLogedIn = await login()
+}
+const cronClass = cron.schedule(cronFormat, action,scheduleOptions);
 
 module.exports = cronClass
 
