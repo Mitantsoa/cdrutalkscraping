623f63ed7fa5523a8ba4dcbb2b7f8c1ae27e9910
unknown <mitantsoa.andrianjaka@gmail.com>
2023-07-23 19:56:19 +0300

    test bot

--- index.js
+++ index.js
@@ -11,27 +11,28 @@ const fs = require('fs')
 config();
 
 app.get('/startcron',async (req,res)=> {
-    if(!cronJson.isrunning){
-        try{
-            logger("Application started");
-            let isLogedIn = await login()
-            cronJson.startDate = moment().format();
-            cronJson.countOfHit = 0;
-            cronJson.isrunning = true;
-            cronJson.countOfHit++;
-        }catch (e) {
-            console.log(e)
-            res.send(e);
-        }
-
-    }else{
-        const cronID = cronJson.countOfHit++
-        logger("Run cron ID:"+cronID);
-        let isLogedIn = await login()
-        cronJson.countOfHit = cronID;
-    }
-    fs.writeFile("./cron.json",cronJson.toString(),"utf-8",()=>{})
-    res.send(cronJson);
+    // if(!cronJson.isrunning){
+    //     try{
+    //         logger("Application started");
+    //         let isLogedIn = await login()
+    //         cronJson.startDate = moment().format();
+    //         cronJson.countOfHit = 0;
+    //         cronJson.isrunning = true;
+    //         cronJson.countOfHit++;
+    //     }catch (e) {
+    //         console.log(e)
+    //         res.send(e);
+    //     }
+    //
+    // }else{
+    //     const cronID = cronJson.countOfHit++
+    //     logger("Run cron ID:"+cronID);
+    //     let isLogedIn = await login()
+    //     cronJson.countOfHit = cronID;
+    // }
+    // fs.writeFile("./cron.json",cronJson.toString(),"utf-8",()=>{})
+    await login()
+    res.send("cron running");
 })
 app.get('/stopcron',async (req,res)=> {
     if(cronJson.isrunning){
